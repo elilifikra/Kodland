11640da891d96ee8c241d30421d63ad3c8269080
elilifikra <elifikradevuk@gmail.com>
2024-09-07 13:42:48 +0300

    Create rastgeleemoji.py

---
+++ rastgeleemoji.py
@@ -0,0 +1,10 @@
+# Bu dersimiz Python Pro dersinin 1. dersidir.
+my_words = ["Hi!", "Python Pro", "Kodland"] 
+print(my_words[1])
+
+# name = input("Lütfen adınızı giriniz: ")
+# print("Hi ", name)
+
+import random
+emojis = ["^^", "0_o", ":)", "¯\\_(ツ)_/¯", "(￢_￢)"]
+print(random.choice(emojis)
